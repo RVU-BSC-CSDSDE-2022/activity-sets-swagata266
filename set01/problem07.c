f2d1d624ee72124d365c27c6676e4e384c1a9dc6
Swagata Singha  Singha <114578729+swagata266@users.noreply.github.com>
2022-12-06 04:31:30 +0000

    problem solve

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,18 +1,36 @@
-//7. Write a C program to find sum of all natural numbers until _n_
-#include<stdio.h>
-int main(void)
+#include <stdio.h>
+
+int input();
+int sum_n_nos(int n);
+void output(int n , int sum);
+
+int main()
 {
-  int a,sum;
-  printf("Enter the number\n");
-  scanf("%d",&a);
-  sum=sum_func(a);
-  printf("The sum of all the numbers till %d is %d",a,sum);
-  return(0);
+  int n,sum=0;
+  n=input();
+  sum=sum_n_nos(n);
+  output(n, sum);
+  return 0;
 }
-int sum_func(int a)
+int input()
 {
-  int sum;
-  sum=(a*(a+1))/2;
+  int n;
+  printf("Enter the range of natural numbers to add\n");
+  scanf("%d", &n);
+  return n;
+}
+
+int sum_n_nos(int n)
+{
+  int sum=0;
+  for(int i=1; i<=n; i++)
+    {
+       sum=sum+i;
+    }
   return sum;
 }
 
+void output(int n, int sum)
+{
+  printf("The sum of %d natural numbers is %d\n", n, sum);
+}
