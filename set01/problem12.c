2c7360f411c23abbd48bf32fbb86039d184d2347
Swagata Singha  Singha <114578729+swagata266@users.noreply.github.com>
2022-12-14 22:19:51 +0000

    problem solve

--- set01/problem12.c
+++ set01/problem12.c
@@ -1,12 +1,66 @@
-//12. Write a C program to find the sum of _n_ complex numbers.
-struct _complex {
-	float real,imaginary;
-};
-typedef struct _complex Complex;
+#include <stdio.h>
 
+struct complex
+{
+   float real;
+   float imaginary;
+};
+typedef struct complex Complex;
 int get_n();
 Complex input_complex();
-void input_n_complex(int n, Complex c[n]);
+void input_n_complex(int n , Complex c[n]);
 Complex add(Complex a, Complex b);
 Complex add_n_complex(int n, Complex c[n]);
 void output(int n, Complex c[n], Complex result);
+
+int main()
+{
+   int n;
+   Complex result={0,0};
+   n=get_n();
+   Complex c[n];
+   input_n_complex(n,c);
+   result=add_n_complex(n,c);
+   return 0;
+}
+int get_n()
+{
+   int n;
+   printf("Enter the size of the array\n");
+  scanf("%d", &n);
+  return n;
+  }
+  
+Complex input_complex()
+{
+  Complex c;
+  printf("Enter the real part\n");
+  scanf("%f", &c.real);
+  printf("Enter the imaginary part\n");
+  scanf("%f", &c.imaginary);
+  return c;
+}
+
+void input_n_complex(int n , Complex c[n])
+{
+  for(int i=0;i<n;i++)
+    {
+           c[i] =input_complex();
+    }
+}
+
+Complex add(Complex a, Complex b)
+{
+
+  a.real=a.real+b.real;
+  a.imaginary=a.imaginary+b.imaginary;
+  return a;
+}
+Complex add_n_complex(int n, Complex c[n])
+{
+  Complex sum={0,0};
+  for(int i=0; i<n; i++)
+    {
+       sum= add(sum, c[i]);
+    }
+}
