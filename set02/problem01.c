bc19b7b5634d5f4412e479add1bb13e2ffc49bd0
Swagata Singha  Singha <114578729+swagata266@users.noreply.github.com>
2022-12-15 15:27:52 +0000

    problem solve

--- set02/problem01.c
+++ set02/problem01.c
@@ -1,12 +1,33 @@
  //Write a program to find the area of a triangle.
-#include <stdio.h>
-void main()
+#include<stdio.h>
+
+void input(float base, float height);
+void find_area(float base, float height, float *area);
+void output(float base, float height, float area);
+
+void input(float base, float height)
 {
-    float base,height;
-    printf("Enter Base and Height: ");
-    scanf("%f %f",&base,&height);
-    float area = (base * height) / 2;
- 
-    //Area with precision of 2 decimal places
-    printf("Area of Triangle is %0.2f",area);
+  float area;
+  printf("Enter the Base of the Triangle\n");
+  scanf("%f",&base);
+  printf("Enter the Height of the Triangle\n");
+  scanf("%f",&height);
+    find_area(base,height,&area);
+  output(base,height,area);
+}
+
+void find_area(float base, float height, float *area)
+{
+   *area=base * height * 0.5;
+}
+void output(float base, float height, float area)
+{
+  printf("The Area of the Triangle with Base %.2f and Height %.2f is %.2f\n",base,height,area);
+}
+int main(void)
+{
+  float base, height;
+  input(base,height);
+   return 0;
+}
 }
