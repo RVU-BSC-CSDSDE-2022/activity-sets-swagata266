41be86e83769092bd9654522a790274bb70113e1
Swagata Singha  Singha <114578729+swagata266@users.noreply.github.com>
2023-01-03 15:19:45 +0000

    problem01.c

--- set05/problem06.c
+++ set05/problem06.c
@@ -1,4 +1,4 @@
-6.  Write a program to find the average of all the odd elements in an array
+//6.  Write a program to find the average of all the odd elements in an array
 #include <stdio.h>
 
 int input_n() {
